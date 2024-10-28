6cae1328aebca571c6fbe85cfcfa45813a608815
fabioElectron <168852075+fabioElectron@users.noreply.github.com>
2024-10-28 09:19:14 +0100

    minor

--- RICADO.Omron/Requests/WriteOperatingModeRequest.cs
+++ RICADO.Omron/Requests/WriteOperatingModeRequest.cs
@@ -2,18 +2,18 @@
 {
     internal class WriteOperatingModeRequest : FINSRequest
     {
-        public WriteOperatingModeRequest(OmronPLC plc, bool run, bool monitor) 
-            : base(plc, (byte)FunctionCodes.OperatingMode, run? (byte)OperatingModeFunctionCodes.RunMode : (byte)OperatingModeFunctionCodes.StopMode)
+        public WriteOperatingModeRequest(OmronPLC plc, bool run, bool monitor)
+            : base(plc, (byte)FunctionCodes.OperatingMode, run ? (byte)OperatingModeFunctionCodes.RunMode : (byte)OperatingModeFunctionCodes.StopMode)
         {
-            if(run)
+            if (run)
             {
-                if(monitor)
+                if (monitor)
                     Body = [0xFF, 0xFF, 0x02];
                 else
                     Body = [0xFF, 0xFF, 0x04];
             }
             else
-            Body = [0xFF,0xFF];
+                Body = [0xFF, 0xFF];
         }
     }
 }
